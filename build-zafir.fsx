91785f61d1199a66d00cfd75f38fb3375b555742
István Gansperger <gansperger@gmail.com>
2015-12-14 12:18:27 +0100

    Add zafir build

---
+++ build-zafir.fsx
@@ -0,0 +1,44 @@
+#load "tools/includes.fsx"
+open IntelliFactory.Build
+
+let bt =
+    BuildTool().PackageId("Zafir.Forms.Bootstrap")
+        .VersionFrom("Zafir", "alpha")
+        .WithFSharpVersion(FSharpVersion.FSharp30)
+        .WithFramework(fun f -> f.Net40)
+
+let main =
+    bt.Zafir.Library("WebSharper.Forms.Bootstrap")
+        .SourcesFromProject()
+        .Embed([])
+        .References(fun r ->
+            [
+                r.NuGet("Zafir.Forms").Latest(allowPreRelease=true).ForceFoundVersion().Reference()
+            ])
+
+let tests =
+    bt.Zafir.SiteletWebsite("WebSharper.Forms.Bootstrap.Tests")
+        .SourcesFromProject()
+        .Embed([])
+        .References(fun r ->
+            [
+                r.Project(main)
+                r.NuGet("Zafir.UI.Next").Reference()
+                r.NuGet("Zafir.Forms").Latest(allowPreRelease=true).Reference()
+            ])
+
+bt.Solution [
+    main
+    tests
+
+    bt.NuGet.CreatePackage()
+        .Configure(fun c ->
+            { c with
+                Title = Some "Zafir.Forms.Bootstrap"
+                LicenseUrl = Some "http://websharper.com/licensing"
+                ProjectUrl = Some "https://github.com/intellifactory/websharper.forms.bootstrap"
+                Description = "A reactive Zafir forms library using Bootstrap"
+                RequiresLicenseAcceptance = true })
+        .Add(main)
+]
+|> bt.Dispatch
